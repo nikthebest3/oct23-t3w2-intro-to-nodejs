90be6d8f9363ab8d063f6b2c741bd146999b0543
Frosty <frosticles922@gmail.com>
2024-04-09 18:46:19 +0800

    added files

--- index.js
+++ index.js
@@ -1,9 +1,14 @@
+require('dotenv').config();
+// The above is the equivalent to the next two lines:
+
+const pokemonPrinterFile = require("./pokemonPrinter");
+
+
+
+console.log(process.env.ENVIRONMENT_MESSAGE);
+
 console.log("Terminal app is running!");
 
-const pokemon = require('pokemon');
-
-let randomPokemon = pokemon.random();
-
-console.log(`Today's lucky Pokemon is ${randomPokemon}`);
+pokemonPrinterFile.pokemonPrinter();
 
 console.log("Bye bye, terminal app finished!");
